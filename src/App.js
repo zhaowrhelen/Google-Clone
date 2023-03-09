2884da991d6c9e091044cdc6adea83f3987b7667
zhaowrhelen <58273143+zhaowrhelen@users.noreply.github.com>
2023-03-09 15:49:55 -0800

    Link tag added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>google clone</h1>
+      <Router>
+        <Routes>
+          <Route
+            path="/search"
+            element={
+              <div>
+                <h1>This is the search page</h1>
+              </div>
+            }
+          />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>this is home page</h1>
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* link tag: no refresh and it does it instantly */}
+          <Link to="/about"> About </Link>
+          <Link to="/store"> Store </Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to="/gmail"> Gmail </Link>
+          <Link to="/images"> Images </Link>
+        </div>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
+}
+
+export default Home;
