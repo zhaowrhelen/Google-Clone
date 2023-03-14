2ae5531b7b38a8c6498d6173b039488c5037be4a
zhaowrhelen <58273143+zhaowrhelen@users.noreply.github.com>
2023-03-14 12:01:02 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="home">
-      <h1>this is home page</h1>
       <div className="home__header">
         <div className="home__headerLeft">
           {/* link tag: no refresh and it does it instantly */}
